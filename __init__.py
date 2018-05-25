e761dbe12eead01cd2da509ecf21ce079197095a
Black <freedomandjustit@gmail.com>
2018-05-25 08:30:26 +0200

    fix setup

--- __init__.py
+++ __init__.py
@@ -1,8 +1,4 @@
-from markdown import markdown
-
 name = "example_pkg"
 
 def joke():
-    return markdown(u'Wenn ist das Nunst\u00fcck git und Slotermeyer?'
-                    u'Ja! ... **Beiherhund** das Oder die Flipperwaldt '
-                    u'gersput.')
+    print('TEST')
